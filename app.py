4b5a0f630688c7b7e99eaf492617b0cf2e614f4e
Odin333t <odinosayande1111@gmail.com>
2025-11-12 21:11:21 +0100

    Update and rename chat4c.py to app.py

--- app.py
+++ app.py
@@ -799,4 +799,5 @@ handler = Mangum(app, lifespan="off")
 # --- Local & Render Dev ---
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
+
     app.run(host='0.0.0.0', port=port)
